93dded7661ea75ecdc9c5f7f6c9eee44b26b086e
Ana Lopez <anai_lopezh@hotmail.com>
2022-03-08 20:38:40 -0600

    actividad1

---
+++ actividad1.js
@@ -0,0 +1,19 @@
+var dato = Number(prompt("ingresa un número"))
+var operacion = dato * 2
+console.log(operacion)
+
+
+var dato1 = Number(prompt("ingresa un número"))
+var dato2 = Number(prompt("ingresa otro número"))
+var operacion = dato1 - dato2
+console.log(operacion)
+
+var radio = 5.3
+var pi = 3.1416
+var area = pi * radio**2
+console.log(area)
+
+var diametroMayor = 8
+var diametroMenor = 6
+var area = diametroMayor * diametroMenor / 2
+console.log(area)
