54f0f6f5a0369960f22b32882af86f1621b2fee8
Ana Lopez <anai_lopezh@hotmail.com>
2022-03-14 20:15:15 -0600

    actividad6

---
+++ actividad6.js
@@ -0,0 +1,28 @@
+
+// ejercicio 1: concatena los arreglos
+var animals = ["eagle", "parrot", "monkey", "boar", "lion"]
+
+var comingSoonAnimals = ["panter", "dragon", "turtle"]
+
+var todosLosAnimales = animals.concat(comingSoonAnimals)
+
+// ejercicio 2: agrega el arreglo de menor a mayor
+
+var arr = [4, 6, 1, 0, 8, 2]
+
+arr.sort(function(a,b){return a-b});
+
+
+//ejercicio 3: agrega un nuevo animal al arreglo
+
+var animals = ["eagle", "parrot", "monkey", "boar", "lion"]
+
+animals.push("cow");
+
+
+//ejercicio 4: retira el primer elemento del arreglo
+
+var animals = ["eagle", "parrot", "monkey", "boar", "lion"]
+
+animals.shift();
+
