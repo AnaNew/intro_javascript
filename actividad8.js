d79313e561de73c6b4d3a5be6a0d09fae088c1e3
Ana Lopez <anai_lopezh@hotmail.com>
2022-03-16 09:35:41 -0600

    actividad8

---
+++ actividad8.js
@@ -0,0 +1,46 @@
+//ejercicio 1
+
+function miFuncion(oracion, palabra){
+   if(palabra === "mundo"){
+       return "true";
+   }else{
+       return "false";
+   }
+}
+miFuncion()
+
+
+// Ejercicio 2
+var numero = [1,2,3,4,10,11]
+
+function otraFuncion(numeros){
+ 
+
+ var suma = (numeros(a + b))
+ console.log(suma)
+}
+otraFuncion(numero)
+
+// Ejercicio 3
+
+function palindromo(word1, word2){
+
+}
+
+// Actividad 4
+
+
+var fahrenheit = 100
+function celcius(Fahrenheit){
+    
+    if(Fahrenheit = Number){
+        console.log (((Fahrenheit - 32)*5) /9) 
+    }
+ function fahrenheit (celcius){
+     if(celcius = Number){
+        console.log((Number *1.8) + 32)
+     }
+    
+ }
+}
+celcius(Fahrenheit);
